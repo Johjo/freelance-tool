d202e78e687eb4be51cdd362d119bfa1f4a94c5e
Jonathan LAURENT <johjo@ytreza.org>
2024-05-31 00:05:58 +0200

    Permet de saisir plusieurs personnes

--- bottle_app.py
+++ bottle_app.py
@@ -13,7 +13,9 @@ def index():
 
 @route('/distribute', method="POST")
 def distribute_controller():
-    distribution = distribute(int(request.POST.budget), Worker(name=request.POST.name, effort=10, coefficient=600))
+    names = [request.POST.name1, request.POST.name2, request.POST.name3, request.POST.name4, request.POST.name5,]
+    workers = [Worker(name=name, effort=10, coefficient=600) for name in names if name != ""]
+    distribution = distribute(int(request.POST.budget), *workers)
     return distribution
 
 application = default_app()
