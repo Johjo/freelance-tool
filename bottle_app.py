21215945c6d728825a490d25cd8215379c299903
Jonathan LAURENT <johjo@ytreza.org>
2024-05-30 23:45:24 +0200

    add calculate page

--- bottle_app.py
+++ bottle_app.py
@@ -1,16 +1,22 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route, run
+from bottle import default_app, route, run, view
 
 from distribute import distribute, Worker
 
 
-@route('/')
-def calculate():
+@route("/")
+@view("index")
+def index():
+    return { }
+
+
+@route('/distribute', method="POST")
+def distribute_controller():
     distribution = distribute(1800, Worker(name="Jonathan", effort=10, coefficient=600))
     return distribution
 
 application = default_app()
 
 if __name__ == "__main__":
-    run()
+    run(reloader=True)
